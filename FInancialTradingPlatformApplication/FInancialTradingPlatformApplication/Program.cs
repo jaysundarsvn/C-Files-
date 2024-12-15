63758752ceccbe5e49e2cdcd11f9694a2c4b9b9c
jaysundarsvn <91945547+jaysundarsvn@users.noreply.github.com>
2024-12-15 15:56:54 +0530

    Add files via upload

---
+++ FInancialTradingPlatformApplication/FInancialTradingPlatformApplication/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+
+//https://chat.openai.com/share/bca103ad-59fd-40a2-be63-b40e79524ecd
+
+namespace FInancialTradingPlatformApplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Method name : Main, ThreadID {Thread.CurrentThread.ManagedThreadId}"); // manages current thread IDs
+             StockMarketTechnicalAnalysis stockMarketTechnicalAnalysisData = new StockMarketTechnicalAnalysis("STZK", new DateTime(2010,1,1), new DateTime(2020,1,1));
+            //constructor calling
+
+            DateTime dateTimeBefore = DateTime.Now;
+
+
+            //THESE ARE SYNCHRONOUS CALL METHODS
+
+            //decimal[] data1 = stockMarketTechnicalAnalysisData.GetOpeningPrices();
+            //decimal[] data2 = stockMarketTechnicalAnalysisData.GetClosingPrices();
+            //decimal[] data3 = stockMarketTechnicalAnalysisData.GetPriceHighs();
+            //decimal[] data4 = stockMarketTechnicalAnalysisData.GetPriceLows();
+            //decimal[] data5 = stockMarketTechnicalAnalysisData.CalculateStockastics();
+            //decimal[] data6 = stockMarketTechnicalAnalysisData.CalculateFastMovingAverage();
+            //decimal[] data7 = stockMarketTechnicalAnalysisData.CalculateSlowMovingAverage();
+            //decimal[] data8 = stockMarketTechnicalAnalysisData.CalculateUpperBoundBollingerBand();
+            //decimal[] data9 = stockMarketTechnicalAnalysisData.CalculateLowerBoundBollingerBand();
+
+            DateTime dateTimeAfter = DateTime.Now;
+            TimeSpan timeSpan = dateTimeAfter.Subtract(dateTimeBefore);
+
+            //THESE AREcASYNCHRONOUS METHODS
+            List<Task<decimal[]>> tasks = new List<Task<decimal[]>>();
+
+            Task<decimal[]> getOpeningPricesTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetOpeningPrices());
+            Task<decimal[]> getClosingPricesTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetClosingPrices());
+            Task<decimal[]> getPricesHighsTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceHighs());
+            Task<decimal[]> getPricesLowsTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceLows());
+            Task<decimal[]> calculateStockasticsTask = Task.Run(() => stockMarketTechnicalAnalysisData.CalculateStockastics());
+            Task<decimal[]> calculateFastMovingAverageTask = Task.Run(() => stockMarketTechnicalAnalysisData.CalculateFastMovingAverage());
+            Task<decimal[]> calculateSlowMovingAverageTask = Task.Run(() => stockMarketTechnicalAnalysisData.CalculateSlowMovingAverage());
+            Task<decimal[]> calculateUpperBollingerBandTask = Task.Run(() => stockMarketTechnicalAnalysisData.CalculateUpperBoundBollingerBand());
+            Task<decimal[]> calculateLowerBollingerBandTask = Task.Run(() => stockMarketTechnicalAnalysisData.CalculateLowerBoundBollingerBand());
+            //Task provides a method to run Tasks (Task.Run()) and a method for waiting for tasks to complete (Task.Wait())
+            //Remeber Task is not a keyword
+            // So basically to write the code as an async method u have to use Task.Run() and give it a function call
+            // followed by a =>
+            // U can write Task normally or for a data type Task<T> T is data type
+
+
+            tasks.Add(getOpeningPricesTask);
+            tasks.Add(getClosingPricesTask);    
+            tasks.Add(getPricesHighsTask);  
+            tasks.Add(getPricesLowsTask);
+            tasks.Add(calculateStockasticsTask);
+            tasks.Add(calculateFastMovingAverageTask);
+            tasks.Add(calculateSlowMovingAverageTask);
+            tasks.Add(calculateUpperBollingerBandTask);
+            tasks.Add(calculateLowerBollingerBandTask);
+
+
+            Task.WaitAll(tasks.ToArray());
+
+            decimal[] data1 = tasks[0].Result;
+            decimal[] data2 = tasks[1].Result;
+            decimal[] data3 = tasks[2].Result;
+            decimal[] data4 = tasks[3].Result;
+            decimal[] data6 = tasks[5].Result;
+            decimal[] data7 = tasks[6].Result;
+            decimal[] data8 = tasks[7].Result;
+            decimal[] data9 = tasks[8].Result;
+
+            Console.WriteLine($"Total time taken for the operations to complete {timeSpan.Seconds} {(timeSpan.Seconds>1 || timeSpan.Seconds < 1 ? "seconds" : "second")}");
+
+            //DisplayDataOnChart(data1,data2, data3, data4);
+
+            Console.ReadKey();
+        }
+    public static void DisplayDataOnChart(decimal[] data1, decimal[] data2, decimal[] data3, decimal[] data4)
+        {
+            Console.WriteLine("Data is displayed on the chart");
+        }
+    }
+
+    public class StockMarketTechnicalAnalysis
+    {
+        public StockMarketTechnicalAnalysis(string StockSymbol, DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            //Code here gets data from remote server
+        }
+
+        public decimal[] GetOpeningPrices() // Gets opening prices
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(GetOpeningPrices)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep( 1000 );
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] GetClosingPrices() // gets closing prices
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(GetClosingPrices)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] GetPriceHighs() // gets price highs
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(GetPriceHighs)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] GetPriceLows() // gets price lows
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(GetPriceLows)}, ThreadId: {Thread.CurrentThread.ManagedThreadId} ");
+            Thread.Sleep(1000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] CalculateStockastics() // calculates stockastics
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(CalculateStockastics)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] CalculateFastMovingAverage() // calcs fast moving averages
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(CalculateFastMovingAverage)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(6000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] CalculateSlowMovingAverage() // calclatrs slow moving averrages
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(CalculateSlowMovingAverage)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(7000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] CalculateUpperBoundBollingerBand() // calculates upper bollinger bound
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(CalculateUpperBoundBollingerBand)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+        public decimal[] CalculateLowerBoundBollingerBand() // calculates lower bollinger bound
+        {
+            decimal[] data;
+            Console.WriteLine($"Method name : {nameof(CalculateLowerBoundBollingerBand)}, ThreadID {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000);
+
+            data = new decimal[] { };
+
+            return data;
+        }
+
+
+    }
+}
---
+++ FieldValidatorAPI/CommonFieldValidatorFunctions.cs
@@ -0,0 +1,128 @@
+﻿using FieldValidatorAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FieldValidatorAPI
+{
+    public delegate bool RequiredValidDel(string fieldVal);
+    public delegate bool StringLengthValidDel(string fieldVal, int min, int max);
+    public delegate bool DateValidDel(string fieldVal, out DateTime date_time);
+    public delegate bool PatternMatchDel(string fieldVal, string pattern);
+    public delegate bool CompareFieldsValidDel(string fieldVal, string FieldValidCompare);
+
+    public class CommonFieldValidatorFunctions
+    {
+        private static RequiredValidDel _requiredValidDel = null;
+        private static StringLengthValidDel _stringLengthValidDel = null;
+        private static DateValidDel _dateValidDel = null;
+        private static PatternMatchDel _patternMatchDel = null;
+        private static CompareFieldsValidDel _compareFieldsValidDel = null;
+
+        public static RequiredValidDel RequiredFieldValidDel
+        {
+            get
+            {
+                if(_requiredValidDel == null)
+                    _requiredValidDel = new RequiredValidDel(RequiredFieldValid);
+                    return _requiredValidDel;
+            }
+        }
+
+        public static StringLengthValidDel stringLengthValid_Del
+        {
+            get
+            {
+                if(_stringLengthValidDel == null)
+                
+                    _stringLengthValidDel = new StringLengthValidDel(StringFieldLengthValid);
+                    return _stringLengthValidDel;
+            }
+        }
+
+        public static DateValidDel DateValid_Del
+        {
+            get
+            {
+                if (_dateValidDel == null)
+                    _dateValidDel = new DateValidDel(DateFieldValidDel);
+                    return _dateValidDel;
+            }
+        }
+
+        public static PatternMatchDel PatternMatch_Del
+        {
+            get
+            {
+                if( _patternMatchDel == null)
+                
+                    _patternMatchDel = new PatternMatchDel(FieldPatternValid);
+                     return _patternMatchDel;
+            }
+        }
+
+        public static CompareFieldsValidDel CompareFieldsValid_Del
+        {
+            get
+            {
+                if (_compareFieldsValidDel == null)
+                    _compareFieldsValidDel = new CompareFieldsValidDel(FieldComparisionValid);
+                    return _compareFieldsValidDel;
+            }
+        }
+
+        private static bool RequiredFieldValid(string FieldVal)
+        {
+            if (!string.IsNullOrEmpty(FieldVal))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool StringFieldLengthValid(string fieldVal, int min, int max)
+        {
+            if (fieldVal.Length > max && fieldVal.Length < min)
+            {
+                return false;
+            }
+
+            else
+            { return true; }
+
+        }
+        private static bool DateFieldValidDel(string fieldVal, out DateTime date_time)
+        {
+            if (DateTime.TryParse(fieldVal, out date_time))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+        private static bool FieldPatternValid(string fieldVal, string regularExpressionPattern)
+        {
+            Regex regex = new Regex(regularExpressionPattern);
+            if (regex.IsMatch(fieldVal))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool FieldComparisionValid(string field1, string field2)
+        {
+            if (field1 == field2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
